0b2ccaa06d532e7fcf4d6d1feaa676d263045864
Luis Miguel Saldarriaga <luis.salda.1995@gmail.com>
2025-02-08 15:40:45 -0500

    Add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import { Analytics } from "@vercel/analytics/react"
 import { SessionProvider } from 'next-auth/react';
 import '../styles/globals.css';
 import PlausibleProvider from 'next-plausible';
@@ -8,6 +9,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
     <SessionProvider session={session}>
       <PlausibleProvider domain='restorephotos.io'>
         <Component {...pageProps} />
+        <Analytics />
       </PlausibleProvider>
     </SessionProvider>
   );
